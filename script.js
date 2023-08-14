c27f96214365a0d60faa777cc49479f7fdd4184e
Nickolas Lopes <nickolaslopes.developer@gmail.com>
2023-08-14 20:28:28 -0300

    todo list

---
+++ script.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('#input')
+const btn = document.querySelector('button')
+const tasks = document.querySelector('#tasks')
+const arrayTasks = []
+function addTask() {
+    arrayTasks.push(input.value)
+    display()
+}
+function display() {
+    
+        let displayTasks = ''
+        arrayTasks.forEach(task => {
+            displayTasks = displayTasks + `
+            <li>
+                <p>${task}</p>
+            </li>
+        `
+        })
+        tasks.innerHTML = displayTasks
+    }
+
+btn.addEventListener('click', addTask)
